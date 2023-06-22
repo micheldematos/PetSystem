fbdc63dfe7f6780eb24ea1b917d544aa2f9fc961
MICHEL DE MATOS <michel.matos@senacsp.edu.br>
2023-06-22 17:15:20 -0300

    Adicionado Front em tela de consulta de animal

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,75 +30,85 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 
-
+// Home
 $routes->get('/', 'Home::index');
-$routes->get("cadC", "Dados::cadastrarC");
-$routes->post("CadastrarC2", "Dados::cadastrarC2");
-$routes->get("cadU","Dados::cadastrarU");
-$routes->post("cadU2","Dados::cadastrarU2");
+
+// Administrador
+$routes->get("ConAdm", "Dados::telaConsultaAdm");
+
+// Animal
 $routes->get("cadA", "Dados::cadastrarAnimal");
 $routes->post("CadA2", "Dados::cadastrarAnimal2");
-$routes->get("cadTipoServ", "Dados::cadastrarTipoServ");
-$routes->post("cadTipoServ", "Dados::cadastrarTipoServ2");
-$routes->get("cadServico", "Dados::cadastrarServico");
-$routes->post("cadServico", "Dados::cadastrarServico2");
-$routes->get("cadOcupacao", "Dados::cadastrarOcupacao");
-$routes->post("cadOcupacao", "Dados::cadastrarOcupacao2");
-$routes->get("cadAtendimento", "Dados::cadastrarAtendimento");
-$routes->post("cadAtendimento", "Dados::cadastrarAtendimento2");
-
-$routes->get("ConAdm", "Dados::telaConsultaAdm");
-$routes->get("ConCli", "Dados::consultaCli");
-$routes->post("ConCli", "Dados::consultaCli2");
 $routes->get("ConAnimal", "Dados::consultaAnimais");
 $routes->post("ConAnimal", "Dados::consultaAnimais2");
-$routes->get("ConUsuario", "Dados::consultaUsuario");
-$routes->post("ConUsuario", "Dados::consultaUsuario2");
-$routes->get("ConOcupacao", "Dados::consultaOcupacao");
-$routes->post("ConOcupacao", "Dados::consultaOcupacao2");
-$routes->get("ConTipoServico", "Dados::consultaTipoServico");
-$routes->post("ConTipoServico", "Dados::consultaTipoServico2");
-$routes->get("ConServico", "Dados::consultaServico");
-$routes->post("ConServico", "Dados::consultaServico2");
-$routes->get("ConAtendimento", "Dados::consultaAtendimento");
-$routes->post("ConAtendimento", "Dados::consultaAtendimento2");
-
-$routes->get("removeOcupacao/(:num)", "Dados::removerOcupacao/$1");
-$routes->post("removeOcupacao/(:num)", "Dados::removerOcupacao/$1");
-$routes->get("removeServico/(:num)", "Dados::removerServico/$1");
-$routes->post("removeServico/(:num)", "Dados::removerServico/$1");
-$routes->get("removeAtendimento/(:num)", "Dados::removerAtendimento/$1");
-$routes->post("removeAtendimento/(:num)", "Dados::removerAtendimento/$1");
-
-$routes->get("AltCliente/(:num)", "Dados::alterarCliente/$1");
-$routes->post("AltCliente/(:num)", "Dados::alterarCliente2");
 $routes->get("AltAnimal/(:num)", "Dados::alterarAnimal/$1");
 $routes->post("AltAnimal/(:num)", "Dados::alterarAnimal2/$1");
-$routes->get("AltUsuario/(:num)", "Dados::alterarUsuario/$1");
-$routes->post("AltUsuario/(:num)", "Dados::alterarUsuario2/$1");
-$routes->get("AltOcupacao/(:num)", "Dados::alterarOcupacao/$1");
-$routes->post("AltOcupacao/(:num)", "Dados::alterarOcupacao2/$1");
-$routes->get("AltOcupacao/(:num)", "Dados::alterarOcupacao/$1");
-$routes->post("AltOcupacao/(:num)", "Dados::alterarOcupacao2/$1");
-$routes->get("AltTipoServico/(:num)", "Dados::alterarTipoServico/$1");
-$routes->post("AltTipoServico/(:num)", "Dados::alterarTipoServico2/$1");
+$routes->get("IntAnimal/(:num)", "Dados::inativarAnimais/$1");
 
-$routes->get("AltServico/(:num)", "Dados::alterarServico/$1");
-$routes->post("AltServico/(:num)", "Dados::alterarServico2/$1");
+// Agendamento
 
 // Atendimento
+$routes->get("cadAtendimento", "Dados::cadastrarAtendimento");
+$routes->post("cadAtendimento", "Dados::cadastrarAtendimento2");
+$routes->get("ConAtendimento", "Dados::consultaAtendimento");
+$routes->post("ConAtendimento", "Dados::consultaAtendimento2");
+$routes->get("removeAtendimento/(:num)", "Dados::removerAtendimento/$1");
+$routes->post("removeAtendimento/(:num)", "Dados::removerAtendimento/$1");
 $routes->get("AltAtendimento/(:num)", "Dados::alterarAtendimento/$1");
 $routes->post("AltAtendimento/(:num)", "Dados::alterarAtendimento2/$1");
 
-// Inativação
-$routes->get("IntAnimal/(:num)", "Dados::inativarAnimais/$1");
+// Cliente
+$routes->get("cadC", "Dados::cadastrarC");
+$routes->post("CadastrarC2", "Dados::cadastrarC2");
+$routes->get("ConCli", "Dados::consultaCli");
+$routes->post("ConCli", "Dados::consultaCli2");
+$routes->get("AltCliente/(:num)", "Dados::alterarCliente/$1");
+$routes->post("AltCliente/(:num)", "Dados::alterarCliente2");
 
-
+// Login
 $routes->post("LoginFC", "Dados::loginCli2");
 $routes->get("LoginFC", "Dados::loginCli2");
 $routes->get("logout", "Dados::logout"); 
 $routes->get("LoginU", "Dados::loginUsuario");
 $routes->post("LoginU", "Dados::loginUsuario2");
+
+// Ocupação
+$routes->get("cadOcupacao", "Dados::cadastrarOcupacao");
+$routes->post("cadOcupacao", "Dados::cadastrarOcupacao2");
+$routes->get("ConOcupacao", "Dados::consultaOcupacao");
+$routes->post("ConOcupacao", "Dados::consultaOcupacao2");
+$routes->get("removeOcupacao/(:num)", "Dados::removerOcupacao/$1");
+$routes->post("removeOcupacao/(:num)", "Dados::removerOcupacao/$1");
+$routes->get("AltOcupacao/(:num)", "Dados::alterarOcupacao/$1");
+$routes->post("AltOcupacao/(:num)", "Dados::alterarOcupacao2/$1");
+$routes->get("AltOcupacao/(:num)", "Dados::alterarOcupacao/$1");
+$routes->post("AltOcupacao/(:num)", "Dados::alterarOcupacao2/$1");
+
+// Serviço
+$routes->get("cadServico", "Dados::cadastrarServico");
+$routes->post("cadServico", "Dados::cadastrarServico2");
+$routes->get("ConServico", "Dados::consultaServico");
+$routes->post("ConServico", "Dados::consultaServico2");
+$routes->get("removeServico/(:num)", "Dados::removerServico/$1");
+$routes->post("removeServico/(:num)", "Dados::removerServico/$1");
+$routes->get("AltServico/(:num)", "Dados::alterarServico/$1");
+$routes->post("AltServico/(:num)", "Dados::alterarServico2/$1");
+
+// Tipo Serviço (Atribuição)
+$routes->get("cadTipoServ", "Dados::cadastrarTipoServ");
+$routes->post("cadTipoServ", "Dados::cadastrarTipoServ2");
+$routes->get("ConTipoServico", "Dados::consultaTipoServico");
+$routes->post("ConTipoServico", "Dados::consultaTipoServico2");
+$routes->get("AltTipoServico/(:num)", "Dados::alterarTipoServico/$1");
+$routes->post("AltTipoServico/(:num)", "Dados::alterarTipoServico2/$1");
+
+// Usuario
+$routes->get("cadU","Dados::cadastrarU");
+$routes->post("cadU2","Dados::cadastrarU2");
+$routes->get("ConUsuario", "Dados::consultaUsuario");
+$routes->post("ConUsuario", "Dados::consultaUsuario2");
+$routes->get("AltUsuario/(:num)", "Dados::alterarUsuario/$1");
+$routes->post("AltUsuario/(:num)", "Dados::alterarUsuario2/$1");
 
 
 /*
