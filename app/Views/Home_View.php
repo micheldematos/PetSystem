cd2b42ae97e38e269d692f1f29cd91d8b3452b38
user_acronus <user_acronus@senacsp.edu.br>
2023-06-23 19:41:06 -0300

    Ajustado responsividade de home

--- app/Views/Home_View.php
+++ app/Views/Home_View.php
@@ -1,15 +1,190 @@
 <?php $session = session(); ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Projeto aula Bootstrap</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="<?php echo base_url("/CSS/estilo.css") ?>">
+
 </head>
-<style>
+
+<body>
+
+  <main class="container-fluid" style="background-color: #F5F9FF;">
+    <!-- Barra de menu -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #0D5CB4;">
+      <div class="container d-flex justify-content-center">
+          <a class="navbar-brand" href="/ProjetoWeb/public/"><img id="logo-cabecalho" src="<?php echo base_url("/IMAGENS/logo.png") ?>"></a>
+          <div class="collapse navbar-collapse" id="navbarNav" style="font-size: 16px;">
+            <div>
+                <ul class="navbar-nav fonte-titulo">
+                    
+                <?php if (!$session->get('Id_Cliente') and !$session->get('Id_Usuario')) { ?>
+                        <a class='link' href='/ProjetoWeb/public/cadC'>Cadastrar</a> &nbsp;
+                        <a class='link' href='/ProjetoWeb/public/LoginFC'>Login </a> &nbsp;";
+                <?php } ?>
+                <?php if ($session->get('Id_Cliente')) { ?>
+                    
+                        <a class='link' href='/ProjetoWeb/public/logout'>Logout</a>";
+
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Cadastro
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/cadA'> Animal</a></li>
+                        </ul>
+                        </li>
+                <?php } ?>
+                <?php if ($session->get('Tipo') == "1") { ?>
+
+                        <a class='link' href='/ProjetoWeb/public/logout'>Logout</a>";
+
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Cadastro
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/cadTipoServ'>Tipo de Serviço</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/cadServico'>Serviço</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/cadOcupacao'> Ocupação</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/cadAtendimento'> Atendimento</a></li>
+                        </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Consulta
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/ConCli'> Cliente</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/ConAnimal'> Animal</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/ConUsuario'> Usuário</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/ConOcupacao'> Ocupação</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/ConTipoServico'> Serviço</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/ConServico'> Atribuição</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/ConAdm'> Horários disponíveis</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/ConAdm'> Agendamento</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/ConAtendimento'> Atendimento</a></li>
+                        </ul>
+                        </li>
+
+                <?php } if ($session->get('Tipo') == "0") { ?>
+                        <a class='link' href='/ProjetoWeb/public/logout'>Logout</a>";
+
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Cadastro
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/cadTipoServ'>Tipo de Serviço</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/cadServico'>Serviço</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/cadOcupacao'> Ocupação</a></li>
+                            <li><a class="dropdown-item" href='/ProjetoWeb/public/cadAtendimento'> Atendimento</a></li>
+                        </ul>
+                        </li>
+                        
+                <?php } ?>
+
+                </ul>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-between align-items-center teste">
+          <a class="" href="/ProjetoWeb/public/" style="padding: 0;"><img id="logo-cabecalho-mobile" src="<?php echo base_url("/IMAGENS/logo.png") ?>"></a>
+          <form class="d-flex justify-content-center" role="search">
+              <button type="button" class="btn btn-outline-light" style="font-weight: bolder; margin-right: 10px; height: 40px;">LOGIN</button>
+              <button type="button" class="btn btn-dark" style="background-color: #DF322E; font-weight: bolder; height: 40px;">AGENDAR</button>
+          </form>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+    </nav>
+    
+    <!-- Conteúdo da página -->
+    <div class="container" style="padding-top: 200px; min-height: 100vh;">
+      
+      <div class="d-flex justify-content-between">
+
+        <div class="container d-flex justify-content-center">
+        </div>
+
+      </div>
+    </div>
+    
+    <!-- Rodapé da página -->
+    <div class="row">
+    
+      <div class="container" style="background-color: #0D5CB4;">
+        <div class="row">
+
+          <div class="col d-flex align-items-center justify-content-center" style="margin-top: 32px;">
+            <img src="<?php echo base_url("/IMAGENS/logo.png") ?>" class="d-block" style="width: 200px;">
+          </div>
+
+          <div class="col col_rodape1">
+            <h5 class="fonte-titulo" style="font-weight: bolder; color: white;">Contatos</h5>
+            <p class="fonte-titulo"><img src="<?php echo base_url("/IMAGENS/telefone.svg") ?>"> (19) 9999-9999</p>
+            <p class="fonte-titulo"><img src="<?php echo base_url("/IMAGENS/whatsapp.svg") ?>"> (19) 9999-9999</p>
+            <p class="fonte-titulo"><img src="<?php echo base_url("/IMAGENS/email.svg") ?>"> contato@bobbarbershop.com.br</p>
+          </div>
+
+          <div class="col col_rodape1">
+            <h5 class="fonte-titulo" style="font-weight: bolder; color: white;">Localização</h5>
+            <p><img src="<?php echo base_url("/IMAGENS/endereco.svg") ?>">  Rua 10, Av. 12, Centro - Rio Claro - SP</p>
+            <h5 class="fonte-titulo" style="font-weight: bolder; color: white;">Redes Sociais</h5>
+            <p><img src="<?php echo base_url("/IMAGENS/facebook.svg") ?>"> <img src="<?php echo base_url("/IMAGENS/instagram.svg") ?>"></p>
+          </div>
+
+          <div class="col col_rodape1">
+            <div class="row">
+              <div class="col">
+                <ul style="list-style: none;">
+                  <li><a href="" style="text-decoration: none; font-weight: bolder; color: white;" class="fonte-titulo">Cliente</a></li>
+                  <li><a href="" style="text-decoration: none; font-weight: bolder; color: white;" class="fonte-titulo" >Animal</a></li>
+                  <li><a href="" style="text-decoration: none; font-weight: bolder; color: white;" class="fonte-titulo" >Usuário</a></li>
+                  <li><a href="" style="text-decoration: none; font-weight: bolder; color: white;" class="fonte-titulo" >Serviços</a></li>
+                </ul>
+              </div>
+    
+              <div class="col ">
+              <ul style="list-style: none;">
+                <li><a href="" style="text-decoration: none; font-weight: bolder; color: white;" class="fonte-titulo">Horários disponíveis</a></li>
+                <li><a href="" style="text-decoration: none; font-weight: bolder; color: white;" class="fonte-titulo" >Atendimento</a></li>
+                <li><a href="" style="text-decoration: none; font-weight: bolder; color: white;" class="fonte-titulo" >Agendamento</a></li>
+                <li><a href="" style="text-decoration: none; font-weight: bolder; color: white;" class="fonte-titulo" >Sobre nós</a></li>
+              </ul>
+              </div>
+            </div>
+          </div>
+          
+        
+          <div class="row">
+          <p class="text-center" style="color: white; font-weight: 100;">© PET SYSTEM 2023. TODOS OS DIREITOS RESERVADOS</p>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    
+  </main>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+<!-- <style>
     * {
         margin: 0px;
         background-color: rgb(230, 223, 223);
@@ -28,41 +203,31 @@
         text-decoration: none;
         background-color: black;
     }
-</style>
+</style> -->
 
-<body>
+<!-- <a href='/app/Controllers/Dados.php'></a> -->
 
-    <header class='topo'>
-        <!-- <a href='/app/Controllers/Dados.php'></a> -->
-        <?php
-
-        if (!$session->get('Id_Cliente') and !$session->get('Id_Usuario')) {
-            echo "<a class='link' href='/ProjetoWeb/public/cadC'>Cadastrar</a> &nbsp;
+<!-- <div class="container" style="padding-top: 200px; min-height: 100vh;">
+        
+        <?php if (!$session->get('Id_Cliente') and !$session->get('Id_Usuario')) { ?>
+                    <a class='link' href='/ProjetoWeb/public/cadC'>Cadastrar</a> &nbsp;
                   <a class='link' href='/ProjetoWeb/public/LoginFC'>Login </a> &nbsp;";
-        }
-        if ($session->get('Id_Cliente')) {
-            echo "<a class='link' href='/ProjetoWeb/public/cadA'> Animal</a> &nbsp;
+        <?php } ?>
+        <?php if ($session->get('Id_Cliente')) { ?>
+            <a class='link' href='/ProjetoWeb/public/cadA'> Animal</a> &nbsp;
                   <a class='link' href='/ProjetoWeb/public/logout'>Logout</a>";
-        }
-        if ($session->get('Tipo') == "1") {
-            echo "<a class='link' href='/ProjetoWeb/public/cadTipoServ'>Tipo de Serviço</a> &nbsp;
+        <?php } ?>
+        <?php if ($session->get('Tipo') == "1") { ?>
+                    <a class='link' href='/ProjetoWeb/public/cadTipoServ'>Tipo de Serviço</a> &nbsp;
                   <a class='link' href='/ProjetoWeb/public/cadServico'>Serviço</a> &nbsp;
                   <a class='link' href='/ProjetoWeb/public/cadOcupacao'> Ocupação</a> &nbsp;
                   <a class='link' href='/ProjetoWeb/public/cadAtendimento'> Atendimento</a> &nbsp;
                   <a class='link' href='/ProjetoWeb/public/ConAdm'> Consultas</a> &nbsp;
                   <a class='link' href='/ProjetoWeb/public/logout'>Logout</a>";
-        }
-        if ($session->get('Tipo') == "0") {
-            echo "<a class='link' href='/ProjetoWeb/public/cadTipoServ'>Tipo de Serviço</a> &nbsp;
+        <?php } if ($session->get('Tipo') == "0") { ?>
+                <a class='link' href='/ProjetoWeb/public/cadTipoServ'>Tipo de Serviço</a> &nbsp;
                   <a class='link' href='/ProjetoWeb/public/cadServico'>Serviço</a> &nbsp;
                   <a class='link' href='/ProjetoWeb/public/cadOcupacao'> Ocupação</a> &nbsp;
                   <a class='link' href='/ProjetoWeb/public/cadAtendimento'> Atendimento</a> &nbsp;
                   <a class='link' href='/ProjetoWeb/public/logout'>Logout</a>";
-        }
-        
-        ?>
-        
-    </header>
-</body>
-
-</html>
+        <?php } ?> -->
